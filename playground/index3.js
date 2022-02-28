0fd3ff727fbaa8554a2a1a6b3d0a0359493b2268
tonyclem <tonyclem001@gmail.com>
2022-02-28 23:38:35 +0100

    added some exercise about filter and map

--- playground/index3.js
+++ playground/index3.js
@@ -1,4 +1,4 @@
-var creatures = [
+const creatures = [
   { name: "Shark", habitat: "Ocean" },
   { name: "Whale", habitat: "Ocean" },
   { name: "Lion", habitat: "Savanna" },
@@ -9,8 +9,10 @@ const arrCreate = creatures.filter((creature) => {
   return creature.habitat === "Ocean";
 });
 
-const arrContainer = arrCreate.map((newCreate) => {
-  return newCreate;
-});
+const arrContainer = arrCreate.map((newCreate) => ({
+  nature: newCreate.name,
+  living: "water",
+  role: "animal",
+}));
 
 console.log(arrContainer);
